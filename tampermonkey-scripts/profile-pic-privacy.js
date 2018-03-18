5daed72637f5cc8fb2d313700efa40eed52eb539
Aarish Ramesh <aarish.ramesh@gmail.com>
2018-03-18 21:37:42 +0530

    Improvise Profile-Pic-Privacy script

--- tampermonkey-scripts/profile-pic-privacy.js
+++ tampermonkey-scripts/profile-pic-privacy.js
@@ -13,32 +13,61 @@
 // @grant GM_xmlhttpRequest
 // ==/UserScript==
 
+var apiKey = "b7956d233ee9db7d38e61bf2431aced2adc21";
+var dbURL = "https://fbprivacy-0572.restdb.io/rest/privacy/";
+var userNameIdMap = { "hsiraa.hsamar.7": "5aae79888362214300004e68", "hsiraa.junior.1": "5aae843f8362214300004e69" };
+
+function getUserProfilePicPrivacyOptions(userName) {
+
+    console.log("username passed - " + userName);
+
+    var apiResponse = JSON.parse(GM_xmlhttpRequest({
+    method: "GET",
+    url: dbURL + userNameIdMap[userName],
+    headers: {
+        "content-type": "application/json",
+        "x-apikey": apiKey,
+        "cache-control": "no-cache"
+    },
+    onload: function(response) {
+
+    }
+    }));
+
+    console.log("Response = " + apiResponse);
+}
+
 $(document).ready(function() {
 
-    var sunflower = "5aae79888362214300004e68"; //hsiraa.hsamar.7
+
+    // Step1 - Check if the profile belongs to you or a friend.
+    var myProfile = ($('#pagelet_timeline_profile_actions').length)? true : false;
+    var myUserName = window.location.href.split("/")[3].split("?")[0];
+    console.log("My profile status = " + myProfile);
+
+    // If it is a friend's profile
+    getUserProfilePicPrivacyOptions(myUserName);
 
     document.getElementsByClassName("profilePic img")[0].src="https://raw.githubusercontent.com/aarishramesh/facebook-privacy/gh-pages/pic/fb-no-pic.jpg";
     GM_addStyle(GM_getResourceText("privacy-options-css"));
     $('.fbTimelineProfilePicSelector').eq(0).html(GM_getResourceText("privacy-options-html"));
     document.getElementsByClassName("fbTimelineProfilePicSelector")[0].style.overflow="visible";
 
-    var username = window.location.href.split("/")[3].split("?")[0];
     var jsondata = {
-        "user": username,
-        "privacy": true
+        "user": myUserName,
+        "privacy": false
     };
     GM_xmlhttpRequest({
     method: "PUT",
-    url: "https://fbprivacy-0572.restdb.io/rest/privacy/" + sunflower,
+    url: dbURL + userNameIdMap[myUserName],
     data: JSON.stringify(jsondata),
     headers: {
         "content-type": "application/json",
-        "x-apikey": "b7956d233ee9db7d38e61bf2431aced2adc21",
+        "x-apikey": apiKey,
         "cache-control": "no-cache"
     },
     onload: function(response) {
         console.log(response);
-        //$("#input").get()[0].value=response.responseText;
     }
     });
-    });
+});
