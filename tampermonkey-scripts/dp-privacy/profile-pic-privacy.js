028b9f3876e577caa67c65dd382325f5eae5ad24
Dwaraka Nath <dtsdwarak@hotmail.com>
2018-04-01 02:54:13 +0530

    [DP Privacy] Long polling for DP Settings

--- tampermonkey-scripts/dp-privacy/profile-pic-privacy.js
+++ tampermonkey-scripts/dp-privacy/profile-pic-privacy.js
@@ -12,23 +12,31 @@
 // @grant GM_getResourceText
 // @grant GM_xmlhttpRequest
 // ==/UserScript==
-
 var apiKey = "b7956d233ee9db7d38e61bf2431aced2adc21";
 var dbURL = "https://fbprivacy-0572.restdb.io/rest/privacy/";
-var userNameIdMap = { "hsiraa.hsamar.7": "5aae79888362214300004e68", "hsiraa.junior.1": "5aae843f8362214300004e69" };
-var fb_dp_privacy_options = { "fb_dp_privacy_false": false, "fb_dp_privacy_true": true };
-var fb_dp_visibility_map = { "Public": 1, "Friends": 2, "Only Me": 3};
-
+var userNameIdMap = {
+    "hsiraa.hsamar.7": "5aae79888362214300004e68",
+    "hsiraa.junior.1": "5aae843f8362214300004e69"
+};
+var fb_dp_privacy_options = {
+    "fb_dp_privacy_false": false,
+    "fb_dp_privacy_true": true
+};
+var fb_dp_visibility_map = {
+    "Public": 1,
+    "Friends": 2,
+    "Only Me": 3
+};
 /*
   This method will help us use a Map bi-directionally.
   Reference - https://stackoverflow.com/a/28191966
 */
 function getKeyByValue(object, value) {
-  return Object.keys(object).find(key => object[key] === value);
+    return Object.keys(object).find(key => object[key] === value);
 }
 
 function fetchUserProfilePicPrivacyOptions(userName) {
-
+    console.log(`Fetching DP privacy options for username - ${userName}`);
     // Use this documentation to parse response -
     // https://wiki.greasespot.net/GM.xmlHttpRequest#Response_Object
     GM_xmlhttpRequest({
@@ -48,15 +56,29 @@ function fetchUserProfilePicPrivacyOptions(userName) {
 function fetchUserProfilePicPrivacyOptionsCallBack(response) {
     var responseValue = JSON.parse(response.responseText);
     if (responseValue.privacy) {
-        document.getElementsByClassName("profilePic img")[0].src="https://raw.githubusercontent.com/aarishramesh/facebook-privacy/master/tampermonkey-scripts/dp-privacy/pic/fb-no-pic.jpg";
+        document.getElementsByClassName("profilePic img")[0].src =
+            "https://raw.githubusercontent.com/aarishramesh/facebook-privacy/master/tampermonkey-scripts/dp-privacy/pic/fb-no-pic.jpg";
     }
 }
 
 function myProfileSettings(userName) {
     GM_addStyle(GM_getResourceText("privacy-options-css"));
     $('.fbTimelineProfilePicSelector').eq(0).html(GM_getResourceText("privacy-options-html"));
-    document.getElementsByClassName("fbTimelineProfilePicSelector")[0].style.overflow="visible";
+    document.getElementsByClassName("fbTimelineProfilePicSelector")[0].style.overflow = "visible";
+    // Bind drop-down buttons to Save actions.
+    $('.fb_dp_privacy_options').click(function() {
+        $('#fb_dp_privacy_selected').html(this.innerHTML);
+        saveDisplayPicPrivacyOptions(userName, fb_dp_privacy_options[this.id], fb_dp_visibility_map[this.innerHTML]);
+    });
+    setInterval(function() {
+        try {
+            setSelectedDropDownOption(userName);
+        } catch (e) {}
+    }, 10000); // check every 10 seconds
+}
 
+function setSelectedDropDownOption(userName) {
+    console.log(`Fetching values for dropdown selected option for username - ${userName}`);
     /*
       This is to set the visibility option in the
       dropdown for the user.
@@ -70,14 +92,9 @@ function myProfileSettings(userName) {
             "cache-control": "no-cache"
         },
         onload: function(response) {
-             var responseValue = JSON.parse(response.responseText);
-             $('#fb_dp_privacy_selected').html(getKeyByValue(fb_dp_visibility_map, responseValue.visibility));
+            var responseValue = JSON.parse(response.responseText);
+            $('#fb_dp_privacy_selected').html(getKeyByValue(fb_dp_visibility_map, responseValue.visibility));
         }
-    });
-
-    $('.fb_dp_privacy_options').click(function() {
-        $('#fb_dp_privacy_selected').html(this.innerHTML);
-        saveDisplayPicPrivacyOptions(userName, fb_dp_privacy_options[this.id], fb_dp_visibility_map[this.innerHTML]);
     });
 }
 
@@ -97,24 +114,26 @@ function saveDisplayPicPrivacyOptions(userName, privacyBool, visibility) {
             "cache-control": "no-cache"
         },
         onload: function(response) {
-            console.log(`Privacy status saved for the username - ${userName} with privacy as - ${privacyBool} and visibility - ${visibility}`);
+            console.log(
+                `Privacy status saved for the username - ${userName} with privacy as - ${privacyBool} and visibility - ${visibility}`
+            );
         }
     });
 }
-
 window.onload = function() {
-
     // Step1 - Check if the profile belongs to you or a friend.
-    var myProfile = (document.getElementById('pagelet_timeline_profile_actions').children.length > 2)? false : true;
+    var myProfile = (document.getElementById('pagelet_timeline_profile_actions').children.length > 2) ? false : true;
     var myUserName = window.location.href.split("/")[3].split("?")[0];
     console.log(`My profile name - ${myUserName}`);
     console.log(`profile mine? - ${myProfile}`);
-
     // If it is a friend's profile
     if (!myProfile) {
-        fetchUserProfilePicPrivacyOptions(myUserName);
+        setInterval(function() {
+            try {
+                fetchUserProfilePicPrivacyOptions(myUserName);
+            } catch (e) {}
+        }, 3000); // check every 3 seconds
     } else {
         myProfileSettings(myUserName);
     }
-
 };
