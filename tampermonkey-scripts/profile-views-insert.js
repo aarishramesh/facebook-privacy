733c4b0a5127355860b5c1779b702973a24adaf0
Aarish Ramesh <aarish.ramesh@gmail.com>
2018-04-01 03:30:19 +0530

    profile view insert changes

--- tampermonkey-scripts/profile-views-insert.js
+++ tampermonkey-scripts/profile-views-insert.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         Profile view
+// @name         Profile view insert
 // @namespace    http://tampermonkey.net/
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        https://www.facebook.com/settings?tab=notifications&section=on_facebook&view
+// @match        https://www.facebook.com/hsiraa.junior.1*friends*
 // @resource style     file:///home/ANT.AMAZON.COM/dwarakam/Desktop/css.css
 // @grant        GM_addStyle
 // @grant GM_getResourceText
@@ -12,38 +12,67 @@
 // ==/UserScript==
 
 var apiKey = "b7956d233ee9db7d38e61bf2431aced2adc21";
-var dbURL = "https://fbprivacy-0572.restdb.io/rest/profilepicviewopt/";
+var profilePicViewUrl = "https://fbprivacy-0572.restdb.io/rest/profilepicview/";
+var profilePicViewOptUrl = "https://fbprivacy-0572.restdb.io/rest/profilepicviewopt/";
 var userNameIdMap = { "hsiraa.hsamar.7": "5aae88388362214300004ee0", "hsiraa.junior.1": "5aae87a08362214300004edc" };
 
-unsafeWindow.pushVal = function () {
-    username = "hsiraa.junior.1";
-    var opt = document.getElementById("profilePrivacyOpt").value;
-    var bool = false;
-    if (opt == 'on')
-        bool = true;
-    var jsondata = {
-        "username": username,
-        "profile_visits_opt": bool
-    };
-    GM_xmlhttpRequest({
-    method: "PUT",
-    url: dbURL + userNameIdMap[username],
-    data: JSON.stringify(jsondata),
-    headers: {
-        "content-type": "application/json",
-        "x-apikey": apiKey,
-        "cache-control": "no-cache"
-    },
-    onload: function(response) {
-        console.log(response);
-    }
-    });
+unsafeWindow.insertVisitor = function () {
+    var username = "hsiraa.junior.1";
+    var profileViewOpt;
+
+    // Check if the privacy option is enabled for the user
+     try {
+          GM_xmlhttpRequest({
+               method: "GET",
+               url: profilePicViewOptUrl + userNameIdMap[username],
+               headers: {'Content-Type': "application/json",
+                         'x-apikey':apiKey,
+                         'cache-control': "no-cache"},
+               onload: function(data){
+                    // check if null return (no results from API)
+                    if (data === null) {  // <-- note that you should use === rather than == for checking against null
+                        console.log('no data!');
+                    } else {
+                       var responseValue = JSON.parse(data.responseText);
+                        console.log(responseValue.profile_visits_opt);
+                        profileViewOpt = responseValue.profile_visits_opt;
+                        console.log(profileViewOpt);
+                        checkPrivacyOptionAndInsert(username, profileViewOpt, profilePicViewUrl);
+                    }
+               },
+               onerror: function(err) {
+                   console.log(err);
+               }
+         });
+    } catch(e) {}
 };
 
-var li = document.createElement('li');
+insertVisitor();
 
-li.setAttribute('id', 'profile_view_notif');
-
-li.innerHTML = '<span><div class="clearfix"><i class="_8o _8s lfloat _ohe img sp_-OqlI68sSK1 sx_58886d"></i><div class="_42ef"><div>Who views my profile ?</div><div class="fsm fwn fcg">Choose whether you want to get notifications when your people visit your profile.</div></div></div></span><span style="width:100px"><div class="_6a _6b"><div class="_6a _6b uiPopover" id="u_1w_l"><select onchange="pushVal()" class="fb_privacy" id="profilePrivacyOpt"><option value="on">On</option><option value="off">Off</option></div></select></span>';
-
-document.getElementsByClassName('uiList _1uyq _4kg _6-j _6-i')[0].appendChild(li);
+function checkPrivacyOptionAndInsert(username, profileViewOpt, dbUrl) {
+    console.log(profileViewOpt);
+    if (profileViewOpt) {
+        var visiteduser = document.getElementsByClassName('_2nlw _2nlv')[0].innerHTML;
+        console.log(visiteduser);
+        var d = new Date();
+        var visitedtime = d.getTime();
+        var jsondata = {
+         "user": username,
+         "visiteduser": visiteduser,
+         "visitedtime" : visitedtime
+        };
+        GM_xmlhttpRequest({
+        method: "POST",
+        url: dbUrl + userNameIdMap[username],
+        data: JSON.stringify(jsondata),
+        headers: {
+         "content-type": "application/json",
+         "x-apikey": apiKey,
+         "cache-control": "no-cache"
+        },
+        onload: function(response) {
+          console.log(response);
+        }
+        });
+    }
+}
